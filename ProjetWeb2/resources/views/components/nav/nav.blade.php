8d99ade3b22b4ec604302d27b27355a9b36d40a8
yanyan90 <yanik_lemieux@hotmail.com>
2023-09-08 16:18:05 -0400

    nav-component

---
+++ ProjetWeb2/resources/views/components/nav/nav.blade.php
@@ -0,0 +1,46 @@
+<div class="nav">
+    <div class="logo">
+        <a href="#"><img src="img/Logo_logo-140-blanc.png" width="133" alt="Logo"></a>
+    </div>
+
+    <div class="liens-nav">
+
+        <div class="home">
+            <div class="span-home">
+                <span class="material-symbols-outlined home">
+                    home
+                </span>
+            </div>
+            <a href="#">
+                Accueil
+            </a>
+        </div>
+
+        <div class="dropdown">
+            <span class="material-symbols-outlined">
+                groups
+            </span>
+            <div class="programmation-dropdown">
+                <p class="programmation">Programmation</p>
+                <div class="dropdown-content">
+                    <a href="#">Horaire</a>
+                    <a href="#">Groupes</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="contact">
+            <span class="material-symbols-outlined">
+                phone_in_talk
+            </span>
+            <a href="#">
+                Contact
+            </a>
+        </div>
+
+        <a class="btn btn-light " href="#">
+            Billets
+        </a>
+    </div>
+</div>
